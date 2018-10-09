a876b0a78a15c09276234c70ed2422414c9762ae
nathalia dias <nathalia.dias3010@gmail.com>
2018-10-08 22:30:40 -0300

    ex novo e o do banco

---
+++ mundo_dois/desafio71.py
@@ -0,0 +1,21 @@
+print('>>>>>>>>>>>>>>>> Banco <<<<<<<<<<<<<<<<<')
+valor = int(input('Quanato deseja sacar?'))
+total = valor
+ced = 50
+totalced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totalced += 1
+    else:
+        if totalced > 0:
+            print(f'Total de {totalced} cédulas de R$ {ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+            totalced = 0
+        if total == 0:
+            break
