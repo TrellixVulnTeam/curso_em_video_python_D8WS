87034b514b040ef271f17ab288c51619cf35f11c
Nathalia <nathalia.dias3010@gmnail.com>
2018-12-11 23:32:50 -0200

    ex novos

---
+++ mundo_tres/ex78.py
@@ -0,0 +1,17 @@
+lista = []
+maior = 0
+menor = 0
+for c in range(0, 5):
+    lista.append(int(input("Digite um valor:")))
+    if c == 0:
+        maior = menor = lista[c]
+    else:
+        if lista[c] > maior:
+            maior = lista[c]
+        if lista[c] < menor:
+            menor = lista[c]
+
+
+print("-="*30)
+print(f"Menor valor: {menor}")
+print(f"Maior valor: {maior}")
